db50e7bd9c36848acaab95a1cc3f4b6abd0a7d72
津村　和人 <185x018x@istc-a-020.epc.center.kobe-u.ac.jp>
2018-05-09 14:51:59 +0900

    task1 submit

--- w10/task1/Isosurfaces.js
+++ w10/task1/Isosurfaces.js
@@ -75,7 +75,8 @@ function Isosurfaces( volume, isovalue )
 
     geometry.computeVertexNormals();
 
-    material.color = new THREE.Color( "white" );
+    //material.color = new THREE.Color( "white" );
+    material.color = new THREE.Color().setHex( cmap[ isovalue ][1] );
 
     return new THREE.Mesh( geometry, material );
 
